2d10ce4f8a11df831b9fabee1bbde82834b62612
jc <chenjiangnan@anban.tech>
2022-03-14 11:40:47 +0800

    add house of force

---
+++ house_of_force/house_of_force.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<unistd.h>
+#include<malloc.h>
+
+#define NAME "house of force\n"
+#define LINE "-------------------------------\n"
+// gcc -no-pie -Wl,-rpath,../libc/glibc_2.28_no-tcache/,-dynamic-linker,../libc/glibc_2.28_no-tcache/ld.so.2 -g house_of_force.c -o house_of_force
+
+void print_banner(void) {
+    printf(NAME);
+}
+
+void print_leak(void) {
+    printf("puts() @ %p\n", &puts);
+    char* a = malloc(0x88);
+    printf("heap @ %p\n", a-0x10);
+    free(a);
+}
+
+void print_option(int malloc_count) {
+    printf("1) malloc %d/4\n", malloc_count);
+    puts("2) target");
+    puts("3) quit");
+    printf("your option is: ");
+}
+
+unsigned long read_num(void) {
+    char buf[31];
+    unsigned long num;
+    read(0, buf, 31);
+    num = strtoul(buf, 0, 10);
+    return num;
+}
+
+void do_malloc(int* malloc_count) {
+    if (*malloc_count <= 3){
+        printf("malloc size: ");
+        char* buf = malloc(read_num());
+        if (buf != NULL){
+            printf("malloc data: ");
+            read(0, buf, malloc_usable_size(buf));
+            *malloc_count+=1;
+            printf(LINE);
+        }
+    }
+    else{
+        printf("Sorry, no more space for you to malloc.\n");
+        printf(LINE);
+    }
+}
+
+char target[] = "hello";
+void do_target(char* target) {
+    printf("The target is :\t%s\n", target);
+    printf(LINE);
+}
+
+void do_exit(void) {
+    printf("exiting...\n");
+    printf(LINE);
+    exit(0);
+}
+
+int main(void) {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    printf(LINE);
+    print_banner();
+    printf(LINE);
+    print_leak();
+    printf(LINE);
+
+    int malloc_count = 0;
+    print_option(malloc_count);
+    unsigned long option_num;
+    // char* target = "Have a nice day!";
+    option_num = read_num();
+    while (true) {
+        switch (option_num) {
+            case 1:
+                do_malloc(&malloc_count);
+                break;
+            case 2:
+                do_target(target);
+                break;
+            case 3:
+                do_exit();
+                break;
+        }   
+        print_option(malloc_count); 
+        option_num = read_num();
+        printf(LINE);
+    }
+    return 0;
+}
